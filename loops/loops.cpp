22f8d39a56f47b64e3fcc8c481a514f2b2021a5d
13krsnaa <saxenakrishna9140@gmail.com>
2026-03-16 09:37:41 +0530

    loops

---
+++ loops/loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // basic loop example
+    for (int i = 0; i <= 5; i++) // loop 5 times
+    {
+        cout << i << "  ";
+    }
+    return 0;
+}
