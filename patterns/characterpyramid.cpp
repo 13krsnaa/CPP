05e667fcbc8dc5708861b5e6f8bc16a31821b1b7
13krsnaa <saxenakrishna9140@gmail.com>
2026-03-26 11:06:28 +0530

    Patterns done

---
+++ patterns/characterpyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 18;
+    char ch = 'A';
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << ch << " ";
+            ch++;
+            if (ch > 'Z')
+            {
+                ch = 'A';
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+};
